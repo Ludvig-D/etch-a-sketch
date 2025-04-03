3f37111f87df5c59ea767461b8400fbb7b05c67e
Ludvig-D <74721111+Ludvig-D@users.noreply.github.com>
2025-04-03 19:39:42 +0200

    Changing color randomly and turns black after 10 swipes

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 const container = document.querySelector('.container');
+const changeSize = document.querySelector('.changeSize');
 
-const size = 16;
+let size = 16;
 
-let width = 100 / 16;
+const color = () => {
+  const red = Math.floor(Math.random() * 255) + 1;
+  const green = Math.floor(Math.random() * 255) + 1;
+  const blue = Math.floor(Math.random() * 255) + 1;
+  let color = `rgba(${red}, ${green}, ${blue}, 1)`;
+  return color;
+};
 
-for (let i = 0; i < size * size; i++) {
-  const div = document.createElement('div');
-  div.style.width = `${width}%`;
-  div.classList.add('divs');
-  container.appendChild(div);
-}
+const flexGrid = (width) => {
+  for (let i = 0; i < size * size; i++) {
+    const div = document.createElement('div');
+    div.style.width = `${100 / width}%`;
+    div.style.opacity = 1;
+    div.addEventListener('mouseenter', () => {
+      div.style.opacity = div.style.opacity - 0.1;
+      div.style.backgroundColor = color();
+    });
+    div.classList.add('divs');
+    container.appendChild(div);
+  }
+};
+
+flexGrid(16);
+
+changeSize.addEventListener('click', () => {
+  size = prompt('Choose between 16 - 100');
+  if (size >= 16 && size <= 100) {
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
+    flexGrid(size);
+  } else {
+    alert('It has to be a number between 16 and 100!!!');
+  }
+});
